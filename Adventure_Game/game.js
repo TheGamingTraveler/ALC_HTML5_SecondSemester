45040c47a3b3144dba47fc1f78eeed5d0065bee9
Michal <michal.r982@stu.nebo.edu>
2019-01-22 10:29:31 -0700

    start of game

---
+++ Adventure_Game/game.js
@@ -0,0 +1,16 @@
+// Single line comment
+/*
+
+multi line comment
+
+*/
+
+//alert("Warning Will Robinson...Warning");
+
+//confirm("do you like digimon?");
+
+//prompt("what kind of digimon do you like?");
+
+var pc = prompt("What is your name outworlder?");
+
+document.write(pc);
