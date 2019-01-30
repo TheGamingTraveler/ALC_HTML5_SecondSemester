fdf0863f7b2f2fae2baa26cc5e0ec66110eb040d
Michal <michal.r982@stu.nebo.edu>
2019-01-30 10:29:47 -0700

    troubleshoot

--- Adventure_Game/game.js
+++ Adventure_Game/game.js
@@ -19,31 +19,46 @@ multi line comment
 Game();
 
 function Game(){
-    alert("Legend of the Helix Crystal");
+    alert("Helix Spire");
     var playerName = prompt("What Is Your Name, Outworlder?");
-    alert("Welcome to the Continent of *"+ playerName);
+    alert("Welcome to the Continent of Utera"+ playerName);
     
     Centeria_Wilds();
     
     function Centeria_Wilds(){
-        var centeriaWilds = prompt("UwU").toLowerCase();
+        var centeriaWilds = prompt("You wake up in the middle of the field. To your understanding of your suuroundings your in a field, meaning that there is at least a town nearby.").toLowerCase();
         
         if(centeriaWilds == "look around" || centeriaWilds == "look"){
-            var centeriaWildsLook = prompt("UwU \n -#");
+            var centeriaWildsLook = prompt("You see a dirt path. When you look at the path you see that it has been recently used, also see holes in the path. \n -go near the holes \n -fallow the path to the right \n -follow the path to the right");
         }
         
-        else if(centeriaWilds == "stop and think" || centeriaWilds == "think"){
-            alert("UwU");
-            var resume = confirm("Do You Wish To Continue?");
+        else if(centeriaWilds == ""){
+            alert("You examine the holes, you feel heat coming off of the holes. You also feel a weird feeling coming off the hole also.");
+            Centeria_Wilds();
+        }
+        
+    }
+    function Town_of_Centeria(){
+        var townOfCenteriaWest = prompt("You have entered Centeria, town of relics and study.")
+        
+        if(townOfCenteriaWest == "talk" || townOfCenteriaWest == "talk to locals"){
+            alert("You try to talk to a local of the town but they cant understand what your saying but you hear them mumble something");
+        }
+        else if(townOfCenteriaWest == "walk around" || townOfCenteriaWest == "walk around and look"){
+            var centeriaPath = prompt("- ask locals \n - keep walking");
             
-            if(resume){
-                Centeria_Wilds();
+            if(centeriaPath == "ask locals"){
+                alert("the local looks at you strangly.");
             }
-            else{
-               alert("Game Over, Thanks For Playing!\(^v^)/"); 
+            else if(centeriaPath == "keep walking" || centeriaPath == "walk"){
+                alert("You keep walking, while on your walk you encounter someone that looks like they're from your world. The stranger spots you and asks you to follow them.");
+                Strangers_House();
             }
         }
-        
+        else{
+            alert("I dont understand" + townOfCenteriaWest);
+            townOfCenteriaWest();
+        }
     }
 }
 
